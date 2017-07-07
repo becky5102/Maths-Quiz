15d63377abfac22e7ec11aa9c1d3ade302be1918
becky5102 <becky100fire@gmail.com>
2017-07-07 14:29:15 +0100

    first xommit

--- README.md
+++ README.md
@@ -26,3 +26,77 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+#Work Experience
+for my work experience , I was given a use case and 5 days to make
+a programme which would give the customer the programme they wanted.
+The use case was to make a Primary maths quiz
+The programme need to have :
+        - 20 random questions
+        - a 2 minute timer to answer them it
+        - the ability to change the difficulty of the questions
+        - the programme needed to be able to mark each answer to give
+        the students a final score
+        - this final score along with the students name and class(school)
+        need to be able to be inputted into a scoreboard (database)
+        - this scoreboard also need to hold previous quiz scores
+
+On first look the project seem quite straight forward but once I began
+looking at the use case in further detail and notices that there is a lot of
+missing pieces of information. For example it only stated that he
+(the customer)is a primary school maths teacher but children in primary can aged
+from 4-11 which would have a big impacted on their maths ability. Also people
+have different maths ability so everyone having the same questions would have it
+too easy for some people but for other it would be too difficult.
+
+At the beginning , it was rather  difficult as I only know python and bits of
+SQL and the program we were writing it in was Typescript and HTML. This was a
+real struggle at the beginning and a lot of google searching helps a bit but
+what help the most was the guidance I got off the company which really helped
+me get started in a new language.
+
+During the programming , there were frustrating moments when it just would not
+work and many google searches didn't help but it was always sort with the help
+of my mentor . But there was also moment when everything perfectly fell into
+place and all of a sudden , other problem could be fixed and it just clicked
+and I knew what I needed to do. Even though there was bits of frustration , I
+wouldn't change any of it as it helped a lot with learning the language and even
+if it didn't solve problem A it would help solve problem B . Also if it wasn't
+challenging it would become rather bored very quickly and would ruin how good
+of a moment it is when everything finally works with any problems or errors.
+
+At the end of my experience , I have learn parts of a new language (typescript)
+and a mark-up language(HTML) and how to upload programmes to git bash and
+firebase . I am sure that these need skills will help me in the future and it
+has also given me experience of what it would be like to work for a programming
+company.
+
+I'm pleased with the programme I have created in 5 days and would do it all
+again in a heart beat . It was excited , enjoyable , creative , challenging and
+at times it is so funny  like it situation when the only thing stopping the
+programme is a full stop in the wrong place or a missing bracket. I have also
+loved the amazing , supporting , friendly atmosphere in the company everyday
+which truly makes this experience so much better.
+
+If I was to improved my programme , I would:
+          - try using more bootstrap to make the user UI look a lot more
+          interesting and more colourful for children.
+          - I would also try and make an external database (using a local one)
+          so the information will be kept longer.
+          - try using new functions to see if it can improve the programme or
+          find new ways to take 20 lines of code into 3-4 lines with it still
+          working in the same way or a better way.
+
+I would also like to try making other similar programmes to learn the language
+(typescript) a lot more as I believe it will be valuable in the future.
+#git bash
+
+`cd maths`
+`ng serve`
+
+#cd for firebase
+cd c:\Users\TrainingUser\maths
+ng build
+firebase deploy
+website = https://maths-work-experience-a5ec0.firebaseapp.com/
+(ctrl +F5 webpage to refresh completely)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,206 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Maths Quiz';
+  showHide: false;
+  counter  = 0
+  ex0 = "PLEASE READ BEFORE STARTNG ..."
+  ex1 = "This is a primary school maths quiz with 20 questions.";
+  ex2 = "You will have 2 minutes to complete as much as you can.";
+  ex3 = "At the end , you will be given a score which you will be able to save into the scoreboard.";
+  ex4 = "Answers must be to 1 decimal place unless the answer is a whole number."
+  p = 0
+  checked = false ;
+  usersInput;
+ diff = 10
+ pick1 = "Easy"
+ diff1(){
+   this.diff = 10
+   this.pick1 = "Easy"
+   //alert("number 1 to 10 selected")
+ }
+
+ diff2(){
+   this.diff = 20
+   this.pick1 = "Medium"
+  // alert("number 1 to 20 selected")
+ }
+
+ diff3(){
+   this.diff = 30
+   this.pick1 = "Hard"
+   //alert("number 1 to 30 selected")
+ }
+ num1 = Math.floor(Math.random() * (this.diff) + 1);
+ num2 = Math.floor(Math.random() * (this.diff) + 1);
+ myArray = ['+', '-', '*',"/"];
+ user =[];
+ rightans =[];
+ questions =[];
+ ended= true;
+ noScore = true;
+ reset = true;
+ data = true;
+ ex = false;
+ noTimer = false;
+ ans = true;
+ showScoreboard = true;
+ start = false;
+ scoreboard = [];
+ name = "";
+ class = "";
+ board = false;
+ options = false;
+ timer;
+
+ timestring;
+ rand = this.myArray[Math.floor(Math.random() * this.myArray.length)];
+pick2 = "+,-,*,/"
+arr1(){
+  this.myArray = ['+', '-']
+  this.pick2 = " +,-"
+  //alert("+,- has been selected")
+}
+
+arr2(){
+  this.myArray = ['+', '-', '*',"/"]
+  this.pick2 = " +,-,*,/"
+
+  //  alert("+,-,*,/ has been selected")
+}
+
+keypress(e){
+ var code = e.keyCode || e.which;
+ if(code == 13){
+   this.Click();
+ }
+}
+
+ Click(){
+   console.log(this.p)
+   if (this.p < 20){
+   this.user.push(this.usersInput)
+   console.log(this.usersInput);
+   this.usersInput =""
+ }
+ this.p++;
+
+ if(this.p == 20){
+   clearInterval(this.timer);
+   this.checkAnswer();
+ }
+}
+
+ createTimer(){
+   this.options = true;
+   this.board = true;
+   this.noTimer = false;
+ this.generateQuestion();
+ this.ans = false;
+ this.start = true;
+ this.reset = false;
+  var cd = 119;
+  var a2 = this;
+  a2.timestring = "2m 0s ";
+
+  this.timer = setInterval(function() {
+  var minutes = Math.floor(cd / 60)
+  var seconds  = cd % 60
+  if (cd < 0 ) {
+    clearInterval(this.timer);
+    a2.checkAnswer();
+  }
+  cd--;
+  a2.timestring =  minutes + "m " + seconds + "s ";
+}, 1000);
+//this.generateQuestion();
+}
+
+score = 0
+  checkAnswer(){
+    var i = 0
+    this.score = 0
+    while (i<20){
+      if (this.user[i] === this.rightans[i] ){
+        this.score++;
+          }
+      else{
+            }
+       i++;
+      }
+
+    this.ended = false;
+    this.noTimer = true;
+    this.noScore = false;
+    this.ex = true;
+    this.options = true;
+    this.ans = true;
+    this.data = false;
+    this.reset = true;
+    this.board = true;
+}
+
+  generateQuestion(){
+    var a = 0
+    while (a<20){
+      this.num1= Math.floor(Math.random() * (this.diff) + 1);
+      this.num2= Math.floor(Math.random() * (this.diff) + 1);
+      this.rand = this.myArray[Math.floor(Math.random() * this.myArray.length)];
+      var quest = this.num1 + this.rand + this.num2
+      var answer = eval(quest)
+      if(answer % 1 !== 0){
+        answer = answer.toFixed(1);
+          }
+      answer = answer.toString();
+      this.questions.push(quest)
+      this.rightans.push(answer)
+      a++;
+      }
+}
+
+  refreshPage(){
+      //  window.location.reload();
+        clearInterval(this.timer)
+        this.p = 0 ;
+        this.noTimer = true;
+        this.score = 0;
+        this.user = [];
+        this.rightans = [];
+        this.questions = [];
+        this.board = false;
+        this.options = false;
+        this.start = false;
+        this.reset = true;
+        this.showScoreboard = true;
+        this.noScore = true;
+        this.ex = false;
+        this.data = true;
+        this.ended = true;
+        this.ans = true;
+
+
+      }
+Scoreboard(){
+  this.noScore = true;
+  this.ended = true;
+  this.ex = true;
+  this.data = true;
+  this.board = true;
+  this.options = true;
+  this.showScoreboard = false;
+  this.reset = false;
+  this.start = true;
+}
+
+Sub2(){
+  this.scoreboard.push({class:this.class , name:this.name , score:this.score})
+  this.data = true;
+  this.noScore = true;
+  console.log(this.scoreboard);
+  console.log(this.name)
+  this.class = "";
+  this.name = "";
+  this.data = true;
+  this.Scoreboard()
+}
 }
